17806e391b0dcb45a6a740940db2d332ed2d7cc4
Priyanshu Shah <105989704+MaDScienTisT21@users.noreply.github.com>
2023-07-21 14:43:04 -0400

    Problem#1

---
+++ Problem#1_2023-21-07.py
@@ -0,0 +1,26 @@
+""" 
+Problem :
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+
+"""
+
+
+def solution(lst, k):
+    for i in range(len(lst)):
+        s = k - lst[i]
+        if s in lst:
+            return True
+    return False
+
+
+# __main__
+lst = [int(x) for x in input("Enter the list of numbers separated by spaces: ").split()]
+k = int(input("Enter the value of k: "))
+#lst = [10, 15, 3, 7]
+#k = 17
+
+print(solution(lst, k))
